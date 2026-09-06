4850f7699dab5f6d4f5581cca22cd3a143ad2be9
Ares 😐 <no.nato051207@gmail.com>
2026-09-06 18:09:42 -0300

    feature: usa api hospedada no azure

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/foundation.dart';
-
+﻿
 class ApiConfig {
   static String get baseUrl {
     const definida = String.fromEnvironment('API_URL');
@@ -8,10 +7,6 @@ class ApiConfig {
       return definida.endsWith('/api') ? definida : '$definida/api';
     }
 
-    if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:8080/api';
-    }
-
-    return 'http://localhost:8080/api';
+    return 'https://evena-api-renato.azurewebsites.net/api';
   }
 }
